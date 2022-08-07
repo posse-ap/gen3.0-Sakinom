f97d8338b011deae65fe85bd437398a974425c38
Sakino Miki <sakinom328@gmail.com>
2022-08-07 15:14:52 +0900

    クイズページをjsでループで表示するように変更

--- quizpage/quiz.js
+++ quizpage/quiz.js
@@ -1,53 +1,181 @@
 'use strict';
 
 {
-  //回答一覧
-  const CORRECT_ANSWERS = [
+  // //回答一覧
+  // const CORRECT_ANSWERS = [
+  //   {
+  //     index: 1,
+  //     value: '約79万人'
+  //   },
+  //   {
+  //     index: 2,
+  //     value: 'X-TECH'
+  //   },
+  //   {
+  //     index: 0,
+  //     value: 'Internet of Things'
+  //   },
+  //   {
+  //     index: 0,
+  //     value: 'Society 5.0'
+  //   },
+  //   {
+  //     index: 0,
+  //     value: 'Web 3.0'
+  //   },
+  //   {
+  //     index: 1,
+  //     value: '約5倍'
+  //   }
+  // ];
+
+  /**
+   * @typedef QUIZ
+   * @property {number} correctNumber 問題番号
+   * @property {string | undefined} note ノート
+   * @property {string} question 問題文
+   * @property {string[]} answers 回答の配列
+   */
+
+  /**
+   * @description 問題と回答の定数
+   * @type {QUIZ[]}
+   */
+   const ALL_QUIZ = [
     {
-      index: 1,
-      value: '約79万人'
+      question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
+      answers: ['約28万人', '約79万人', '約183万人'],
+      correctNumber: 1,
+      note: '経済産業省 2019年3月 － IT 人材需給に関する調査'
     },
     {
-      index: 2,
-      value: 'X-TECH'
+      question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
+      answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
+      correctNumber: 2,
     },
     {
-      index: 0,
-      value: 'Internet of Things'
+      question: 'IoTとは何の略でしょう？',
+      answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
+      correctNumber: 0,
     },
     {
-      index: 0,
-      value: 'Society 5.0'
+      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+      answers: ['Society 5.0', 'CyPhy', 'SDGs'],
+      correctNumber: 0,
+      note: 'Society5.0 - 科学技術政策 - 内閣府'
     },
     {
-      index: 0,
-      value: 'Web 3.0'
+      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+      answers: ['Web3.0', 'NFT', 'メタバース'],
+      correctNumber: 0,
     },
     {
-      index: 1,
-      value: '約5倍'
+      question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
+      answers: ['約2倍', '約5倍', '約11倍'],
+      correctNumber: 1,
+      note: 'Accenture Technology Vision 2021'
     }
   ];
 
-  //全ての問題を取得
+  /**
+   * @description クイズコンテナーの取得
+   * @type {HTMLElement}
+   */
+  const quizContainer = document.getElementById('js-quizContainer');
+
+  /**
+   * @description クイズ１つ１つのHTMLを生成するための関数
+   * @param quizItem { QUIZ }
+   * @param questionNumber { number }
+   * @returns {string}
+   */
+   const createQuizHtml = (quizItem, questionNumber) => {
+    const answersHtml = quizItem.answers.map((answer, answerIndex) => 
+      `<li class="p-quiz-box_answer_item">
+      <button class="p-quiz-box_answer_button js-answer" data-answer="${answerIndex}">
+        ${answer}<i class="u-icon_arrow"></i>
+      </button>
+    </li>`
+    ).join('');
+
+    // 引用テキストの生成
+    const noteHtml = quizItem.note ? `<cite class="p-quiz-box_note">
+      <i class="u-icon_note"></i>${quizItem.note}
+    </cite>` : '';
+
+    return `<section class="p-quiz-box js-quiz" data-quiz="${questionNumber}">
+    <div class="p-quiz-box_question">
+      <h2 class="p-quiz-box_question_title">
+        <span class="p-quiz-box_label">Q${questionNumber + 1}</span>
+        <span
+          class="p-quiz-box_question_title_text">${quizItem.question}</span>
+      </h2>
+      <figure class="p-quiz-box_question_image">
+        <img src="../assets-ph1-website-main/img/quiz/img-quiz0${questionNumber + 1}.png" alt="">
+      </figure>
+    </div>
+    <div class="p-quiz-box_answer">
+      <span class="p-quiz-box_label p-quiz-box_label--accent">A</span>
+      <ul class="p-quiz-box_answer_list">
+        ${answersHtml}
+      </ul>
+      <div class="p-quiz-box_answer_correct js-answerBox">
+        <p class="p-quiz-box_answer_correct_title js-answerTitle"></p>
+        <p class="p-quiz-box_answer_correct_content">
+          <span class="p-quiz-box_answer_correct_content_label">A</span>
+          <span class="js-answerText"></span>
+        </p>
+      </div>
+    </div>
+    ${noteHtml}
+  </section>`
+   }
+
+   /**
+   * @type {string}
+   * @description 生成したクイズのHTMLを #js-quizContainer に挿入
+   */
+  quizContainer.innerHTML = ALL_QUIZ.map((quizItem, index) => {
+    return createQuizHtml(quizItem, index)
+  }).join('')
+
+  /**
+   * @type {NodeListOf<Element>}
+   * @description すべての問題を取得
+   */
   const allQuiz = document.querySelectorAll('.js-quiz');
 
-  //buttonタグにdisabledを付与
+  /**
+   * @description buttonタグにdisabledを付与
+   * @param answers {NodeListOf<Element>}
+   */
   const setDisabled = answers => {
     answers.forEach(answer => {
       answer.disabled = true;
     })
   }
 
-  // trueかfalseで出力する文字列を出し分ける
+  /**
+   * @description trueかfalseで出力する文字列を出し分ける
+   * @param target {Element}
+   * @param isCorrect {boolean}
+   */
   const setTitle = (target, isCorrect) => {
     target.innerText = isCorrect ? '正解' : '不正解...';
   }
+
+  /**
+   * @description trueかfalseでクラス名を付け分ける
+   * @param target {Element}
+   * @param isCorrect {boolean}
+   */
   const setClassName = (target, isCorrect) => {
     target.classList.add(isCorrect ? 'is-correct' : 'is-incorrect');
   }
 
-  //各問題の中での処理
+  /**
+   * 各問題の中での処理
+   */
   allQuiz.forEach(quiz => {
     const answers = quiz.querySelectorAll('.js-answer');
     const selectedQuiz = Number(quiz.getAttribute('data-quiz'));
@@ -64,10 +192,10 @@
         setDisabled(answers);
 
         // 正解ならtrue, 不正解ならfalseをcheckCorrectに格納
-        const isCorrect = CORRECT_ANSWERS[selectedQuiz].index === selectedAnswer;
+        const isCorrect = ALL_QUIZ[selectedQuiz].index === selectedAnswer;
 
         // 回答欄にテキストやclass名を付与
-        answerText.innerText = CORRECT_ANSWERS[selectedQuiz].value;
+        answerText.innerText = ALL_QUIZ[selectedQuiz].value;
         setTitle(answerTitle, isCorrect);
         setClassName(answerBox, isCorrect);
       })
