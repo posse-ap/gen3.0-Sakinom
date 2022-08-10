00033607dd88b962d1c2e8b8a93be2fe3a6cf20e
Sakino Miki <sakinom328@gmail.com>
2022-08-10 20:07:51 +0900

    問題ランダム出力

--- quizpage/quiz.js
+++ quizpage/quiz.js
@@ -43,33 +43,39 @@
    */
    const ALL_QUIZ = [
     {
+      id: 1,
       question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
       answers: ['約28万人', '約79万人', '約183万人'],
       correctNumber: 1,
       note: '経済産業省 2019年3月 － IT 人材需給に関する調査'
     },
     {
+      id: 2,
       question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
       answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
       correctNumber: 2,
     },
     {
+      id: 3,
       question: 'IoTとは何の略でしょう？',
       answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
       correctNumber: 0,
     },
     {
+      id: 4,
       question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
       answers: ['Society 5.0', 'CyPhy', 'SDGs'],
       correctNumber: 0,
       note: 'Society5.0 - 科学技術政策 - 内閣府'
     },
     {
+      id: 5,
       question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
       answers: ['Web3.0', 'NFT', 'メタバース'],
       correctNumber: 0,
     },
     {
+      id: 6,
       question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
       answers: ['約2倍', '約5倍', '約11倍'],
       correctNumber: 1,
@@ -132,10 +138,30 @@
    }
 
    /**
+   * @description 配列の並び替え
+   * @param arrays {Array}
+   * @returns {Array}
+   */
+  const shuffle = arrays => {
+    const array = arrays.slice();
+    for (let i = array.length-1; i >= 0; i--){
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+    }
+    return array
+  }
+
+  /**
+   * @description quizArrayに並び替えたクイズを格納
+   * @type {Array}
+   */
+   const quizArray = shuffle(ALL_QUIZ)
+
+   /**
    * @type {string}
    * @description 生成したクイズのHTMLを #js-quizContainer に挿入
    */
-  quizContainer.innerHTML = ALL_QUIZ.map((quizItem, index) => {
+  quizContainer.innerHTML = quizArray.map((quizItem, index) => {
     return createQuizHtml(quizItem, index)
   }).join('')
 
@@ -192,10 +218,11 @@
         setDisabled(answers);
 
         // 正解ならtrue, 不正解ならfalseをcheckCorrectに格納
-        const isCorrect = ALL_QUIZ[selectedQuiz].index === selectedAnswer;
+        const correctNumber = quizArray[selectedQuiz].correctNumber
+        const isCorrect = correctNumber === selectedAnswer;
 
         // 回答欄にテキストやclass名を付与
-        answerText.innerText = ALL_QUIZ[selectedQuiz].value;
+        answerText.innerText = quizArray[selectedQuiz].answers[correctNumber];
         setTitle(answerTitle, isCorrect);
         setClassName(answerBox, isCorrect);
       })
